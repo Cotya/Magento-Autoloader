bb449769a180fa4499c59886f07e5454ead27688
Flyingmana <flyingmana@googlemail.com>
2014-12-13 03:33:28 +0100

    add first basic test

--- tests/Basic/CaseTest.php
+++ tests/Basic/CaseTest.php
@@ -10,4 +10,10 @@ class CaseTest extends \PHPUnit_Framework_TestCase
         $model = \Mage::getModel('core/url');
         $this->assertTrue(!!$model);
     }
+
+    public function testGetAutoloaderModel()
+    {
+        $model = \Mage::getModel('psr0autoloader/Observer');
+        $this->assertTrue(!!$model);
+    }
 }
